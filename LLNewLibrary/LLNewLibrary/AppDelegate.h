83c5e6571732d7c2ef814b18ec5acdef78b58401
王丽丽 <wanglili@2345.com>
2016-09-28 14:50:30 +0800

    新建工程

---
+++ LLNewLibrary/LLNewLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLNewLibrary
+//
+//  Created by wanglili on 2016/9/28.
+//  Copyright © 2016年 wanglili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LLNewLibrary/LLNewLibrary/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LLNewLibrary
+//
+//  Created by wanglili on 2016/9/28.
+//  Copyright © 2016年 wanglili. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
